1af29df1bdbe79a0f6f61671019ed84c53d8b8e4
djeylani Aïdara <djeylani.aidara@epitech.eu>
2026-06-19 00:54:23 +0200

    feat(init_windows()): rename the title of the game Pygame window to MewPoly

--- init_windows.py
+++ init_windows.py
@@ -3,6 +3,7 @@ import pygame
 def init_windows():
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
+    pygame.display.set_caption("MewPoly")
     clock = pygame.time.Clock()
     running = True
     return screen, clock,running
