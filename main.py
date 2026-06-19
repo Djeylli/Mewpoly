d3a48e4158b04ae34df7e868e865f4e0525f1d76
Djeylani Aidara <djeylani.aidara@epitech.eu>
2026-06-19 11:25:05 +0200

    move: I move init_windows.py, main_loop.py and main.py in Src/

--- main.py
+++
@@ -1,9 +0,0 @@
-from init_windows import init_windows
-from main_loop import main_loop
-
-def main():
-    screen, clock, running = init_windows()
-    main_loop(screen, clock, running)
-
-if __name__ == '__main__':
-    main()
