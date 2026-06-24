f6f16a5a2fca8a4386599740c3a3391aef99105c
djeylani Aïdara <djeylani.aidara@epitech.eu>
2026-06-24 02:35:28 +0200

    feat: i add player class with arrow key move

---
+++ Src/Include/player.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Player():
+
+    def __init__(self, pos, size, color, speed):
+        self.pos = pygame.math.Vector2(pos)
+        self.size = pygame.math.Vector2(size)
+        self.color = color(*color)
+        self.speed = 4(speed)
+
+
+    def Arrow(self):
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.pos.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.pos.x += self.speed
+        if keys[pygame.K_UP]:
+            self.pos.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.pos.y += self.speed
+
+
+
+
