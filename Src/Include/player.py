8e13720444008a7f51ad2f1aac72c4fbc9033e2a
djeylani Aïdara <djeylani.aidara@epitech.eu>
2026-06-27 14:37:29 +0200

    feat: i create a class player

--- Src/Include/player.py
+++ Src/Include/player.py
@@ -2,26 +2,36 @@ import pygame
 
 class Player():
 
-    def __init__(self, pos, size, color, speed):
-        self.pos = pygame.math.Vector2(pos)
+    def __init__(self, pos, size, skin, speed, health, strength, attack, defense, name, level, power, money):
         self.size = pygame.math.Vector2(size)
-        self.color = color(*color)
-        self.speed = 4(speed)
+        self.speed = speed
+        self.health = health
+        self.strength = strength
+        self.attack = attack
+        self.defense = defense
+        self.name = name
+        self.level = level
+        self.power = power
+        self.money = money
+        self.skin = pygame.image.load(skin).convert()
+        self.rect = self.skin.get_rect()
+        self.rect.topleft = pos
+        self.frame = pygame.Rect(0, 0, self.size.x, self.size.y)
 
 
-    def Arrow(self):
+    def move(self):
 
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
-            self.pos.x -= self.speed
+            self.rect.x -= self.speed
         if keys[pygame.K_RIGHT]:
-            self.pos.x += self.speed
+            self.rect.x += self.speed
         if keys[pygame.K_UP]:
-            self.pos.y -= self.speed
+            self.rect.y -= self.speed
         if keys[pygame.K_DOWN]:
-            self.pos.y += self.speed
+            self.rect.y += self.speed
 
-
-
+    def draw(self, screen):
+        screen.blit(self.skin, self.rect, self.frame)
 
